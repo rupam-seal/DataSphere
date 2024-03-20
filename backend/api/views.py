9afad714b74ee0eb731140b66a7936b6fcaa9fd9
rupam-seal <rupam.x.seal@gmail.com>
2024-03-20 22:13:14 +0530

    🐛fix(backend): page size and menu icon

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,7 +7,7 @@ from .serializers import InsightSerializer
 class CustomPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    max_page_size = len(Insight.objects.all())
 
 @api_view(['GET'])
 def api_overview(request):
